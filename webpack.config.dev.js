ccd93570eaf68dea5184522ea61da84f00a8fc59
michael-mason <cptshiner@gmail.com>
2018-04-13 14:27:36 +1000

    update license and dev webpack config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -22,7 +22,7 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: ['css-loader', 'sass-loader'],
       },
     ],
   },
